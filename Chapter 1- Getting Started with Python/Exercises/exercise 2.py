1a80bc685f6058ce9c69a6eef8f7ef662d76dea8
SeanKenev <144330063+SeanKenev@users.noreply.github.com>
2023-10-04 12:13:50 +0400

    exercise 2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 2.py
@@ -0,0 +1,23 @@
+print ("Name" , ":Sean " ,12345)
+print ("Fathers Name" , ": Wilson Patano" , 2570)
+print ("abc" , "xyz" , 13232)
+
+name = "sean"
+name = "Bathspa"
+
+print(name)
+
+print (19/2)
+
+name =input("Enter Name: ")
+print(name )
+
+age + input("Enter age : ")
+print (age)
+
+print (22+5)
+print (2570-257)
+print(752*27)
+
+print ((2*5/7+(27+5)))
+print (int (ans))
